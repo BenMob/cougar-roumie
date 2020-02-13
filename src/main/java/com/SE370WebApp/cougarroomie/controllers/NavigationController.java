3a499fdaf4fe7a6e80133e9ffdebdf61450630f0
Dreger Dietle <dregerdietle@gmail.com>
2020-02-13 08:10:07 -0800

    remove test import
    remove unused import

--- src/main/java/com/SE370WebApp/cougarroomie/controllers/NavigationController.java
+++ src/main/java/com/SE370WebApp/cougarroomie/controllers/NavigationController.java
@@ -1,6 +1,5 @@
 package com.SE370WebApp.cougarroomie.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
