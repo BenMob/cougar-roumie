898ed7553a1a010ed89496cb50df9b6bcc270d59
Dreger Dietle <dregerdietle@gmail.com>
2020-03-16 18:38:59 -0700

    BareBones
    Complete Refresh with Spring Security

--- src/main/resources/static/firebase/authState.js
+++
@@ -1,8 +0,0 @@
-
-firebase.auth().onAuthStateChanged(function(user) { // this will be called every time the status changes logged in/logged out
-    if (user) {
-        if (location.pathname === "/") {setupNav(user);} // The page is home, so set links to logged in..
-    } else {
-        if (location.pathname === "/") {setupNav();} // The page is home, so set links to logged out..
-    }
-});
