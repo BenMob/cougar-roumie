084acfb9fb5bfd733813ad3734a4ce663cd4bf9e
Benjamin <omben001@cougars.csusm.edu>
2020-03-31 00:35:22 -0700

    Adds a view model for the first time user's form

---
+++ src/main/java/com/SE370/Cougar/Roomie/view/FirstTimeLoginForm.java
@@ -0,0 +1,25 @@
+package com.SE370.Cougar.Roomie.view;
+
+public class FirstTimeLoginForm {
+
+    String first_name;
+    String last_name;
+    String gender;
+
+    public String getLast_name() {
+        return last_name;
+    }
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+    public String getFirst_name() {
+        return first_name;
+    }
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+    public String getGender() { return gender; }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
