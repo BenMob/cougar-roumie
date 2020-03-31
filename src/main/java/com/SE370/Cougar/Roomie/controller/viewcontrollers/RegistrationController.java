43554e5d1a2b7104c57044de2d3b6967aded8d62
Benjamin <omben001@cougars.csusm.edu>
2020-03-31 03:40:23 -0700

    Adds a form handling logic

--- src/main/java/com/SE370/Cougar/Roomie/controller/viewcontrollers/RegistrationController.java
+++ src/main/java/com/SE370/Cougar/Roomie/controller/viewcontrollers/RegistrationController.java
@@ -1,13 +1,18 @@
 package com.SE370.Cougar.Roomie.controller.viewcontrollers;
 
 import com.SE370.Cougar.Roomie.controller.services.CustomUserDetailsService;
+import com.SE370.Cougar.Roomie.model.CustomUserDetails;
+import com.SE370.Cougar.Roomie.view.AssessmentForm;
+import com.SE370.Cougar.Roomie.view.FirstTimeLoginForm;
 import com.SE370.Cougar.Roomie.view.RegistrationForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
@@ -26,6 +31,26 @@ public class RegistrationController {
         userService.createNewUser(registrationForm);
         return "registrationSuccess";
     }
+
+    @GetMapping("/user/secondaryinfo")
+    public String showSecondaryInfoForm(WebRequest request, Model model){
+        model.addAttribute("secondaryInfoForm", new FirstTimeLoginForm());
+        return "secondaryinfo";
+    }
+
+    @PostMapping("/user/secondaryinfo")
+    public String registerFirstLastGender(@ModelAttribute FirstTimeLoginForm secondaryInfoForm ) {
+
+        int userId = (((CustomUserDetails) SecurityContextHolder
+                .getContext().getAuthentication().getPrincipal())
+                .getUser_id());
+
+        userService.updateFirstTimeUser(secondaryInfoForm, userId);
+        return "assessment";
+    }
+
+
+    // This will be the PUT request handler for the at secondaryinfo.html
 /*
     @PutMapping(value = "/{userId}",
             consumes={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
