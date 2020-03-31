e9c929450dfa84e5323ecbe8e6fd28e5e4ee3225
Dreger Dietle <dregerdietle@gmail.com>
2020-03-30 23:00:18 -0700

    Test Messaging Service

---
+++ src/main/java/com/SE370/Cougar/Roomie/controller/viewcontrollers/ChatController.java
@@ -0,0 +1,45 @@
+package com.SE370.Cougar.Roomie.controller.viewcontrollers;
+
+import com.SE370.Cougar.Roomie.model.Message;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import static java.lang.String.format;
+
+@Controller
+public class ChatController {
+    private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @GetMapping("/chat")
+    public String chat() {
+        return "chat";
+    }
+
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public Message sendMessage(@Payload Message chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public Message addUser(@Payload Message chatMessage,
+                               SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
