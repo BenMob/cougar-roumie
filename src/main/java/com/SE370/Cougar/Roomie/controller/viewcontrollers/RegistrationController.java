6f434ed21843023c70a205260e5a101a1bb2a233
Benjamin <omben001@cougars.csusm.edu>
2020-03-31 00:38:02 -0700

    Trying to add a method that handles PUT requests, not working yet

--- src/main/java/com/SE370/Cougar/Roomie/controller/viewcontrollers/RegistrationController.java
+++ src/main/java/com/SE370/Cougar/Roomie/controller/viewcontrollers/RegistrationController.java
@@ -1,13 +1,14 @@
 package com.SE370.Cougar.Roomie.controller.viewcontrollers;
 
 import com.SE370.Cougar.Roomie.controller.auth.CustomUserDetailsService;
+import com.SE370.Cougar.Roomie.model.entities.User;
+import com.SE370.Cougar.Roomie.view.FirstTimeLoginForm;
 import com.SE370.Cougar.Roomie.view.RegistrationForm;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.WebRequest;
 
 @Controller
@@ -26,7 +27,13 @@ public class RegistrationController {
         userService.createNewUser(registrationForm);
         return "registrationSuccess";
     }
+/*
+    @PutMapping(value = "/{userId}",
+            consumes={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+            produces ={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    public String updateUserInfo(@PathVariable String userId, @RequestBody FirstTimeLoginForm userInfo){
 
-
-
+        return "Success!!!!!";
+    }
+ */
 }
