5ebbba5c3836e97f3362399a890dd7132cd49516
Benjamin <omben001@cougars.csusm.edu>
2020-04-03 20:45:19 -0700

    Calls utility function from User

--- src/main/java/com/SE370/Cougar/Roomie/controller/services/UserService.java
+++ src/main/java/com/SE370/Cougar/Roomie/controller/services/UserService.java
@@ -62,22 +62,9 @@ public class UserService implements UserDetailsService {
 
     @Transactional
     public User updateFirstTimeUser(FirstTimeLoginForm secondaryInfoForm){
-
-        // TODO: Clean this code up
-
-        /******************************************************************************
-         CustomUserDetails customUser =  (((CustomUserDetails) SecurityContextHolder
-               .getContext().getAuthentication().getPrincipal()));
-
-         If the casting below is hard to understand, the customUser above is what is being
-         passed in as a parameter in the new User() object below.
-        *******************************************************************************/
         User user = new User((((CustomUserDetails) SecurityContextHolder
                 .getContext().getAuthentication().getPrincipal())));
-
-        user.setFirstName(secondaryInfoForm.getFirst_name());
-        user.setLastName(secondaryInfoForm.getLast_name());
-        user.setGender(secondaryInfoForm.getGender());
+        user.registerFormSecondary(secondaryInfoForm);
 
         return userRepository.save(user);
     }
